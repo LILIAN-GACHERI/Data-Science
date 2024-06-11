e01069f67e92dca101f52ec54547202a593356fb
LILIAN-GACHERI <167884016+LILIAN-GACHERI@users.noreply.github.com>
2024-06-11 21:20:02 +0300

    Add files via upload

---
+++ titanic_test .py
@@ -0,0 +1,97 @@
+import pandas as pd
+import seaborn as sns
+titanic_df =sns.load_dataset('titanic')
+titanic_df.head()
+print(titanic_df.head())
+print(titanic_df.shape)
+print(titanic_df.info())
+print(titanic_df.columns)
+print(titanic_df.dtypes)
+print(titanic_df.isna().sum())
+import numpy as np
+import matplotlib.pyplot as plt
+sns.catplot(x='sex', data=titanic_df, kind='count')
+plt.show()
+sns.catplot(x='pclass', data=titanic_df, hue='sex', kind='count')
+plt.show()
+sns.catplot(x='pclass', data=titanic_df, hue='who', kind='count')
+plt.show()
+titanic_df.age.hist(bins=80)
+plt.show()
+fig = sns.FacetGrid(titanic_df, hue="sex", aspect=4)
+fig.map(sns.kdeplot, 'age', shade=True)
+
+oldest = titanic_df['age'].max()
+
+fig.set(xlim=(0, oldest))
+
+fig.add_legend()
+plt.show()
+fig = sns.FacetGrid(titanic_df, hue="who",aspect=4)
+fig.map(sns.kdeplot, 'age', shade=True)
+
+oldest = titanic_df['age'].max()
+
+fig.set(xlim=(0, oldest))
+
+fig.add_legend()
+plt.show()
+fig = sns.FacetGrid(titanic_df, hue="pclass",aspect=4)
+fig.map(sns.kdeplot, 'age', shade=True)
+
+oldest = titanic_df['age'].max()
+
+fig.set(xlim=(0, oldest))
+
+fig.add_legend()
+plt.show()
+sns.catplot(x='deck', data=titanic_df, kind='count', hue='deck')
+plt.show()
+sns.catplot(x='embarked', data=titanic_df, hue='pclass', kind='count', order=['C', 'Q', 'S'])
+plt.show()
+sns.catplot(x='alone', data=titanic_df, kind='count', hue='alone')
+plt.show()
+sns.catplot(x='alive', data=titanic_df, kind='count')
+plt.show()
+sns.catplot(x='pclass', data=titanic_df, y='alive', kind='point')
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
